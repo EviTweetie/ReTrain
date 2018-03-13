8c23d16c5341c4bd5aa3e2aa673474a307968b51
EviTweetie <evitweetie@gmail.com>
2018-03-13 22:35:43 +0100

    first commit

---
+++ src/components/Root.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Root extends Component {
+  render() {
+    return (
+      <div className="Root">
+        <h1>Hello from Root</h1>
+      </div>
+    );
+  }
+}
+
+export default Root;
